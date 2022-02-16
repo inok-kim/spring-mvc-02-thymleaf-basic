31669c19d8e9394cda3744a6e3448a7913147466
inok-kim <iokim1220@gmail.com>
2022-02-16 15:48:02 +0900

    # thymleaf 기본

--- src/main/java/hello/thymleaf/ThymleafBasicApplication.java
+++ src/main/java/hello/thymleaf/ThymleafBasicApplication.java
@@ -1,4 +1,4 @@
-package hello.thymleafbasic;
+package hello.thymleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/hello/thymleaf/ThymleafBasicApplicationTests.java
+++ src/test/java/hello/thymleaf/ThymleafBasicApplicationTests.java
@@ -1,4 +1,4 @@
-package hello.thymleafbasic;
+package hello.thymleaf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
